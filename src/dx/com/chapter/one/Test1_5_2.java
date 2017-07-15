b24e94c2281723d0a7b517957bd3ecc2e8a3b05e
NobiGo <nobigogle@gmail.com>
2017-07-15 10:04:27 +0800

    Test1_5_2

---
+++ src/dx/com/chapter/one/Test1_5_2.java
@@ -0,0 +1,11 @@
+package dx.com.chapter.one;
+
+/**
+ * Created by dx on 2017/7/15.
+ */
+public class Test1_5_2 {
+    public static void main(String[] args) {
+        float value = -5;
+        System.out.println(Integer.toBinaryString(Float.floatToIntBits(value)));
+    }
+}
